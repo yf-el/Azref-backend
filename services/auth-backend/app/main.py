4309d0bf93eeabb6453085ce866d85cf14de0f9a
personal <youssefelouarrat1995@gmail.com>
2026-05-24 14:25:47 +0200

    add CORSMiddleware

--- services/auth-backend/app/main.py
+++ services/auth-backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, CORSMiddleware
 
 from app.config import settings
 
@@ -6,6 +6,24 @@ app = FastAPI(
     title="Azref auth-backend",
     version=settings.service_version,
 )
+cors = settings.cors
+
+if cors:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=cors.allow_origins,
+        allow_methods=cors.allow_methods,
+        allow_headers=cors.allow_headers,
+        allow_credentials=cors.allow_credentials,
+    )
+else:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
+    )
 
 
 @app.get("/health")
